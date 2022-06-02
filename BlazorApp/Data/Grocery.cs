7c7ec79b3fe8d1f5a5089015417380c1db78341d
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-06-02 20:01:27 +0430

    Layout

--- BlazorApp/Data/Grocery.cs
+++ BlazorApp/Data/Grocery.cs
@@ -1,7 +1,12 @@
-﻿namespace BlazorApp.Data;
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorApp.Data;
 
 public class Grocery
 {
+    [Required]
+    [StringLength(15,ErrorMessage ="Name should be less than 15 characters.")]
     public string Name { get; set; }
+    [Required]
+    [Range(1,10000,ErrorMessage ="Valid Price Range is (1 and 10000)")]
     public float Price { get; set; }
 }
