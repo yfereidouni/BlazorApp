5cc759a445aece9ed5d2d00e8442657cc1b9d086
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-06-03 12:20:57 +0430

    Call API

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -6,6 +6,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient
+//{
+//    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+//});
+
+builder.Services.AddScoped(
+    sp => new HttpClient
+    {
+        BaseAddress = new Uri("https://goweather.herokuapp.com/weather/")
+    });
+
 
 await builder.Build().RunAsync();
